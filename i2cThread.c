90e2817a7c12bf4ef8b55eeab9edd0ccc06c2f19
calluminrfa <callum.braines@infraglo.com>
2018-10-30 16:23:27 +0000

    Made temp sample 1/s and added rounding

--- i2cThread.c
+++ i2cThread.c
@@ -62,45 +62,20 @@ void i2CThread(void *pvParameters) {
 
   /// Write configuration to SC18IS602B
   taskENTER_CRITICAL();
-  // Config
+  // Setup Thermocouple
   dataBuffer[0] = 0xf0;
   dataBuffer[1] = 0x2;
   err = i2c_slave_write(I2C_BUS, SC18IS602B, NULL, &dataBuffer, 2);
   if (err != 0) {
     printf("Couldn't set SPI int\r\n");
   }
-  dataBuffer[0] = 0x1;
-  dataBuffer[1] = 0xFF;
-  dataBuffer[2] = 0xFF;
-  err = i2c_slave_write(I2C_BUS, SC18IS602B, NULL, &dataBuffer, 3);
-  if (err != 0) {
-    printf("Couldn't init SPI read\r\n");
-  }
-  taskEXIT_CRITICAL();
-
-  vTaskDelay(4);
-
-  taskENTER_CRITICAL();
-  uint8_t tempBuffer[8] = {1};
-  err = i2c_slave_read(I2C_BUS, SC18IS602B, NULL, &tempBuffer, 2);
-  if (err != 0) {
-    printf("Couldn't read SPI buffer\r\n");
-  } else {
-    uint16_t tempInt = tempBuffer[0];
-    tempInt = tempInt << 8;
-    tempInt = tempInt | tempBuffer[1];
-    tempInt = tempInt >> 3;
-    float temp = tempInt * 0.25;
-    printf("%2x, %2x\r\n", tempBuffer[0], tempBuffer[1]);
-    printf("Temp = %.2f degrees\r\n", temp);
-  }
-
   taskEXIT_CRITICAL();
 
   struct I2CVarsStruct *rxI2CVars;
   extern QueueHandle_t xI2CQueue;
 
   while (1) {
+    static uint8_t tempIttCounter = 0;
     if (xQueueReceive(xI2CQueue, &(rxI2CVars), 0)) {
       // uint8_t testVar1 = pollButtonsI2C();
       taskENTER_CRITICAL();
@@ -135,6 +110,41 @@ void i2CThread(void *pvParameters) {
     }
 
     taskEXIT_CRITICAL();
+
+    if (tempIttCounter >= 50) {
+      taskENTER_CRITICAL();
+      dataBuffer[0] = 0x1;
+      dataBuffer[1] = 0xFF;
+      dataBuffer[2] = 0xFF;
+      err = i2c_slave_write(I2C_BUS, SC18IS602B, NULL, &dataBuffer, 3);
+      if (err != 0) {
+        printf("Couldn't init SPI read\r\n");
+      }
+      taskEXIT_CRITICAL();
+
+      vTaskDelay(4);
+
+      taskENTER_CRITICAL();
+
+      uint8_t tempBuffer[8] = {1};
+      err = i2c_slave_read(I2C_BUS, SC18IS602B, NULL, &tempBuffer, 2);
+      if (err != 0) {
+        printf("Couldn't read SPI buffer\r\n");
+      } else {
+        uint16_t tempInt = tempBuffer[0];
+        tempInt = tempInt << 8;
+        tempInt = tempInt | tempBuffer[1];
+        tempInt = tempInt >> 3;
+        float temp = tempInt * 0.25;
+        tempInt = (int)temp;
+        printf("Temp = %d degrees\r\n", tempInt);
+      }
+      taskEXIT_CRITICAL();
+      tempIttCounter = 0;
+    } else {
+      tempIttCounter++;
+    }
+
     // static uint8_t *pDataBuffer;
   }
 }
